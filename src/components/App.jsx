567f79f4e81ca2d63cf5f134cc83ed8691b19349
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-05-08 22:21:03 +0200

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddContactForm from './addContactForm/addContactForm';
 import ContactList from './contactList/contactList';
 import Filter from './filter/filter';
 
 const App = () => {
-  const [state, setState] = useState({
-    contacts: [],
-    filter: '',
+  const [contacts, setContacts] = useState(() => {
+    return localStorage.getItem('contacts')
+      ? [...JSON.parse(localStorage.getItem('contacts'))]
+      : [];
   });
-
-  const setContacts = useCallback(() => {
-    setState(prev => {
-      return {
-        ...prev,
-        contacts: JSON.parse(localStorage.getItem('contacts')) || [],
-      };
-    });
-  }, []);
+  const [filter, setFilter] = useState('');
 
   const setContactToStorage = newContact => {
-    const { contacts } = state;
     if (
       contacts.some(contact => {
         return (
@@ -30,25 +22,15 @@ const App = () => {
       alert(`${newContact.name} is already in contacts`);
       return;
     } else {
-      localStorage.setItem(
-        'contacts',
-        JSON.stringify([...contacts, newContact])
-      );
-      setContacts();
+      setContacts(prev => [...prev, newContact]);
     }
   };
 
   const handleChange = e => {
-    const { contacts } = state;
-
-    setState({
-      [e.target.name]: e.target.value,
-      contacts,
-    });
+    setFilter(e.target.value);
   };
 
   const getFilteredContacts = () => {
-    const { contacts, filter } = state;
     return contacts.length
       ? contacts.filter(contact => {
           return contact.name.toLowerCase().includes(filter.toLowerCase());
@@ -57,18 +39,14 @@ const App = () => {
   };
 
   const deleteContact = deletedId => {
-    const { contacts } = state;
-
     const filteredContacts = contacts.filter(({ id }) => deletedId !== id);
-    localStorage.setItem('contacts', JSON.stringify(filteredContacts));
-    setContacts();
+    setContacts(filteredContacts);
   };
 
   useEffect(() => {
-    setContacts();
-  }, [setContacts]);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  const { contacts, filter } = state;
   return (
     <div>
       <h1>Phonebook</h1>
